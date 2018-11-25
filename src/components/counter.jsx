3b642a3413ba77239322a9388a62724b630901ca
Charles Burch <georgia_beast89@yahoo.com>
2018-11-25 17:56:24 -0600

    conditional render element on page

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,33 +6,25 @@ class Counter extends Component {
 
      };
 
-    render() { 
-
+     renderTags = () => {
         const { tags } = this.state;
-        return ( 
-            <React.Fragment>
-                <span className={this.getBadgeClasses()} >{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
+
+        if(tags.length === 0) return <p>There are no tags!</p>;
+        return (   <ul>
                     {tags.map(tag =>
                         <li key={tag}>{tag}</li>
                     )}
                 </ul>
+                )
+     }
+    render() { 
+        return ( 
+            <React.Fragment>
+                {this.renderTags()}
             </React.Fragment>
         );
     }
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
-       
-        return classes;
-    }
-
-    formatCount() {
-        const { value } = this.state;
-        return value === 0 ? 'Zero': value;
-        // return value === 0 ? <h1>Zero</h1>: value;
-    }
+ 
 }
  
 export default Counter;
