b2bf7c1022dcdbaeb6ee300178b212e6fefd3d61
Charles Burch <georgia_beast89@yahoo.com>
2018-11-25 15:03:09 -0600

    inline style test

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,15 +4,11 @@ class Counter extends Component {
         value: 0
      }
 
-     style = {
-         fontSize: 50,
-         fontWeight: "bold"
-     }
     render() { 
         return ( 
             <React.Fragment>
                 {/* span.badge.badge-primary.m-2 */}
-                <span style={this.style} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
