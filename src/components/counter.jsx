e8e28cf9774f6e066c7c1bef0ed2033c32ce91c9
Charles Burch <georgia_beast89@yahoo.com>
2018-11-25 15:51:24 -0600

    adding class dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,14 @@ class Counter extends Component {
      }
 
     render() { 
+
+        let classes = "badge m-2 badge-";
+        classes += (this.state.value === 0) ? "warning" : "primary";
+
         return ( 
             <React.Fragment>
                 {/* span.badge.badge-primary.m-2 */}
-                <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span className={classes} >{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
@@ -18,6 +22,8 @@ class Counter extends Component {
         return value === 0 ? 'Zero': value;
         // return value === 0 ? <h1>Zero</h1>: value;
     }
+
+
 }
  
 export default Counter;
