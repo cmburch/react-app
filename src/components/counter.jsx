a92110bd455d4406f2b0e84e72ede4b0bf58010d
Charles Burch <georgia_beast89@yahoo.com>
2018-11-24 20:40:28 -0600

    create first parent component

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Test</h1>
+                <button>Increment</button>
+            </div>
+        );
+    }
+}
+ 
+export default Counter;
