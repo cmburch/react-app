f62428b7c4c6f7e369c55e8b14ce088bf8515cb7
Charles Burch <georgia_beast89@yahoo.com>
2018-11-25 16:08:38 -0600

    refactor added class dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,25 +5,25 @@ class Counter extends Component {
      }
 
     render() { 
-
-        let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
-
         return ( 
             <React.Fragment>
-                {/* span.badge.badge-primary.m-2 */}
-                <span className={classes} >{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()} >{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.value === 0) ? "warning" : "primary";
+       
+        return classes;
+    }
+
     formatCount() {
         const { value } = this.state;
         return value === 0 ? 'Zero': value;
         // return value === 0 ? <h1>Zero</h1>: value;
     }
-
-
 }
  
 export default Counter;
