3226fee14b602fad245d8182a5b60dc48c9300cc
Charles Burch <georgia_beast89@yahoo.com>
2018-11-25 14:44:19 -0600

    inline style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,11 +3,17 @@ class Counter extends Component {
     state = { 
         value: 0
      }
+
+     style = {
+         fontSize: 50,
+         fontWeight: "bold"
+     }
     render() { 
         return ( 
             <React.Fragment>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                {/* span.badge.badge-primary.m-2 */}
+                <span style={this.style} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
