d6bb89c00a68c1acd91b1de33b25821f28d14244
Charles Burch <georgia_beast89@yahoo.com>
2018-11-25 19:49:17 -0600

    handle events

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,27 +6,37 @@ class Counter extends Component {
 
      };
 
-     renderTags = () => {
-        const { tags } = this.state;
+     handleIncrement = () => {
+         console.log('Increment Clicked')
+     }
 
-        if(tags.length === 0) return <p>There are no tags!</p>;
-        return (   <ul>
+    render() { 
+
+        const { tags } = this.state;
+        return ( 
+            <React.Fragment>
+                <span className={this.getBadgeClasses()} >{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
                     {tags.map(tag =>
                         <li key={tag}>{tag}</li>
                     )}
                 </ul>
-                )
-     }
-    render() { 
-        return ( 
-            <React.Fragment>
-                {/* compare a boolen && string the string will display if first condition is true */}
-                {this.state.tags.length === 0 && "Please create a new tag" }
-                {this.renderTags()}
             </React.Fragment>
         );
     }
- 
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.value === 0) ? "warning" : "primary";
+       
+        return classes;
+    }
+
+    formatCount() {
+        const { value } = this.state;
+        return value === 0 ? 'Zero': value;
+        // return value === 0 ? <h1>Zero</h1>: value;
+    }
 }
  
 export default Counter;
